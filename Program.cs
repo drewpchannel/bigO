6802f6f83e2a6b75fdbd792597984bc447f5bf92
drewpchannel <drewpcchannel@gmail.com>
2017-01-26 16:37:04 -0500

    first example

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testbig0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            int[] makeAnArray = new int[userNumber];
+            for (var x = 0; x < userNumber; x++)
+            {
+                makeAnArray[x] = x; 
+            }
+            DisplayIterations(makeAnArray);
+        }
+        static void DisplayIterations(int[] testA)
+        {
+            int totalIterations = 0;
+            for (var i = 0; i < testA.Length; i++)
+            {
+                totalIterations++;
+                Console.WriteLine("i is " + i );
+                for (var j = i + 1; j < testA.Length; j++)
+                {
+                    totalIterations++;
+                    Console.WriteLine("j is " + j);
+                }
+            }
+            Console.WriteLine("The amount of iterations: " + totalIterations);
+        }
+    }
+}
